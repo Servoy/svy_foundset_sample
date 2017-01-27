b077bddec0272295210abe6cc9d782da353a974a
emera <emera@servoy.com>
2017-01-27 12:33:48 +0200

    font awsome instead of buttons

---
+++ svy_foundset_sample/forms/detail_view.js
@@ -0,0 +1,17 @@
+/**
+ * @param {JSEvent} event
+ *
+ * @properties={typeid:24,uuid:"8F7A0EC6-7CB9-44E0-8DCC-5B7B113FECFD"}
+ */
+function onAdd(event) {
+	foundset.newRecord();
+}
+
+/**
+ * @param {JSEvent} event
+ *
+ * @properties={typeid:24,uuid:"3F2C86EF-A488-4530-90D3-308EA40C6798"}
+ */
+function onDelete(event) {
+	foundset.deleteRecord();
+}
